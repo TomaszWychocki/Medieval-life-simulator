d2b46c358165cbf1dd13ba3d61dade6b69cb220f
Corey <corey.a.tess@gmail.com>
2018-10-03 15:02:01 -0400

    added hospital class and health to the miner class

---
+++ script/hospital.js
@@ -0,0 +1,38 @@
+class Hospital {
+    constructor(x, y)
+    {
+      this.posX = x;
+      this.posY = y;
+      this.maxPatients = 5;
+      this.patients = [];
+    }
+
+    addPatient(patient)
+    {
+      if (this.maxPatients > 5 && patient.health > 100)
+      {
+        this.patients.push(patient);
+      }
+    }
+
+    healPatient(patient) {
+      if(patient.health > 100) {
+        patient.health++;
+      }
+      return patient;
+    }
+
+    releasePatient(patient) {
+      this.patients.pop(patient);
+    }
+
+    draw()
+    {
+      noStroke();
+      fill(244, 65, 80);
+      ellipse(this.posX, this.posY, 20, 20);
+      fill(255);
+      textAlign(CENTER);
+      text('HOSPITAL', this.posX, this.posY - 12);
+    }
+  }
--- script/miner.js
+++ script/miner.js
@@ -1,6 +1,6 @@
-class Miner 
+class Miner
 {
-    constructor(x, y, _mine, _blacksmith) 
+    constructor(x, y, _mine, _blacksmith)
     {
         this.posX = x;
         this.posY = y;
@@ -9,11 +9,12 @@ class Miner
         this.iron = 0;
         this.state = 0; // 0 - go to the mine | 1 - dig iron | 2 - go to the blacksmith
         this.speed = 5;
+        this.health = 100;
     }
 
-    action() 
+    action()
     {
-        if(this.state == 0) 
+        if(this.state == 0)
         {
             let nextPoint = getNextPoint(this.posX, this.posY, this.mine.posX, this.mine.posY, this.speed);
 
@@ -26,7 +27,7 @@ class Miner
             }
         }
 
-        if(this.state == 1) 
+        if(this.state == 1)
         {
             this.mine.dig();
 
@@ -38,14 +39,14 @@ class Miner
             }
         }
 
-        if(this.state == 2) 
+        if(this.state == 2)
         {
             let nextPoint = getNextPoint(this.posX, this.posY, this.blacksmith.posX, this.blacksmith.posY, this.speed);
 
             this.posX = nextPoint[0];
             this.posY = nextPoint[1];
 
-            if(distanceTo(this.posX,this.posY,this.blacksmith.posX,this.blacksmith.posY) < 15) 
+            if(distanceTo(this.posX,this.posY,this.blacksmith.posX,this.blacksmith.posY) < 15)
             {
                 this.blacksmith.iron += this.iron;
                 this.iron = 0;
@@ -63,7 +64,7 @@ class Miner
         textAlign(CENTER);
         text('MINER', this.posX, this.posY - 12);
 
-        if(this.iron > 0) 
+        if(this.iron > 0)
         {
             fill(0);
             ellipse(this.posX - 2, this.posY - 2, 4, 4);

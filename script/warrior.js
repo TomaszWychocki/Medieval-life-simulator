cc012519c30ce2b644567ca1890564b9851e6976
au12113 <wasupont@windowslive.com>
2018-10-06 18:18:09 +0700

    modify warrior
    - warrior fixed only one blacksmith to pick a sword
    - warrior must has sword to damage a enemy
    - warrior's sword is breakable
    - a little bit mess when I try to pass blacksmith to warrior constructor

--- script/warrior.js
+++ script/warrior.js
@@ -1,11 +1,13 @@
 class Warrior extends Villager
 {
-	constructor(x, y, barrak, health = 100, defense = 75)
+	constructor(x, y, barrak, blacksmith, health = 100, defense = 75)
 	{
 		super(x, y);
 		this.defense = defense;
 		this.speed = 5;
 		this.barrak = barrak;
+		this.sword_durable = 0; 
+		this.blacksmith = blacksmith;
 	}
 
 	checkForEnemies()
@@ -15,6 +17,7 @@ class Warrior extends Villager
 			var dist = distanceTo(this.posX, this.posY, enemy.posX, enemy.posY);
 			if (dist < 15)
 			{
+				this.sword_durable = constrain(this.sword_durable - SWORD_DEGRADE, 0, SWORD_DURABLE);
 				enemy.health -= 10;
 				enemy.displayText = "OUCH!";
 				setTimeout(() => enemy.displayText = `ENEMY (${enemy.health})`, 1500);
@@ -26,19 +29,34 @@ class Warrior extends Villager
 	{
 		this.barrak.warriorDied();
 	}
-
-	draw()
+	
+	move() 
 	{
-		this.checkForEnemies();
-
-		if (distanceTo(this.posX, this.posY, this.direction[0], this.direction[1]) < 10)
+		if(this.sword_durable == 0) 
 		{
-			this.direction = [random(width), random(height)];
+			this.direction = [this.blacksmith.posX, this.blacksmith.posY];
+			if(this.blacksmith.swords > 0)
+			{
+				this.blacksmith.swords--;
+				this.sword_durable = SWORD_DURABLE;
+			}
 		}
+		else{
+			this.checkForEnemies();
+			if (distanceTo(this.posX, this.posY, this.direction[0], this.direction[1]) < 10)
+			{
+				this.direction = [random(width), random(height)];
+			}
+		} 
 
 		var point = getNextPoint(this.posX, this.posY, this.direction[0], this.direction[1], this.speed);
 		this.posX = point[0];
 		this.posY = point[1];
+	}
+
+	draw()
+	{
+		this.move() 
 
 		noStroke();
 		fill(214, 66, 126);
@@ -46,5 +64,10 @@ class Warrior extends Villager
 		fill(255);
 		textAlign(CENTER);
 		text('Warrior', this.posX, this.posY - 12);
+		//
+		push()
+			textSize(10)
+			text('DUR: '+this.sword_durable, this.posX, this.posY - 30);
+		pop()
 	}
 }
